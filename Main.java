c0e41cd025b603fdeb222ecd396258e85b8b0f9f
Priyanka dhongde <shineprincess456@gmail.com>
2021-02-15 08:11:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Leap Year!!...");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a year :");
+    int year = sc.nextInt();
+
+    boolean leap = false;
+
+    if(year % 4 == 0){
+      if(year % 100 == 0) {
+        if(year % 400 == 0)
+          leap=true;
+        else
+          leap = false;
+      
+      } else
+          leap = true;
+    
+    } else
+        leap = false;
+    
+    if(leap) 
+      System.out.println(year + "  is a leap year.");
+    
+    else 
+      System.out.println(year + "  is not a leap year.");
+    
+  }
+}
